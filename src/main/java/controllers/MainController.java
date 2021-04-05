e0003b37fd16937ad16aa83ab9a4671caa171b02
Wendily <wendilyrr@gmail.com>
2021-04-04 23:59:50 -0400

    vista usuario y localStorage

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -40,6 +40,7 @@ public class MainController {
                     ctx.render("public/index.html");
                 });
 
+
             });
 
             path("/authenticate", () -> {
@@ -97,6 +98,14 @@ public class MainController {
                     String academico = ctx.formParam("selected");
 
                     // Form form = new Form("","",);
+                });
+
+
+            });
+
+            path("/user", ()->{
+                get("/", ctx -> {
+                    ctx.render("/templates/userNew.html");
                 });
 
 
